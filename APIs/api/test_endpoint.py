c5e55b2f6dd581a0d3dfdbc30ff4c16e64becc4d
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-08-19 22:24:16 +0300

    Test endpoint adds a question [Finished #159834832]

--- APIs/api/test_endpoint.py
+++ APIs/api/test_endpoint.py
@@ -1,5 +1,6 @@
 import unittest
 from api_endpoint import app
+import json
 
 
 class TestAllApiEndPoints(unittest.TestCase):
@@ -15,8 +16,16 @@ class TestAllApiEndPoints(unittest.TestCase):
 
     def test_get_specific_question(self):
         """Test method that fetches specific question."""
-        response = self.client.get("http://localhst:5000/api/v1/questions/1")
+        response = self.client.get("http://localhost:5000/api/v1/questions/1")
         self.assertEquals(response.status_code, 200)
+
+    def test_post_a_question(self):
+        """Test method that posts a question"""
+        sample = {"id": 9, "title": "How do endpoints work?"}
+        response = self.client.post("http://localhost:5000/api/v1/questions",
+                                    data = json.dumps(sample),
+                                    content_type = 'application/json')
+        self.assertAlmostEquals(response.status_code, 200)
 
 
 if __name__ == '__main__':
