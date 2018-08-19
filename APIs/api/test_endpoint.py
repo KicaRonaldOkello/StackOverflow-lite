4003cb3582eba4256a0be85e5425fee469afc5e5
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-08-20 02:59:46 +0300

    Test endpoint that adds answer [Finished #159834856]

--- APIs/api/test_endpoint.py
+++ APIs/api/test_endpoint.py
@@ -1,6 +1,7 @@
 import unittest
-from api_endpoint import app
 import json
+from api_endpoint import app
+
 
 
 class TestAllApiEndPoints(unittest.TestCase):
@@ -11,21 +12,29 @@ class TestAllApiEndPoints(unittest.TestCase):
 
     def test_get_all_questions(self):
         """Test method that fetches all questons."""
-        response = self.client.get("http://localhost:5000/api/v1/questions")
+        response = self.client.get("/api/v1/questions")
         self.assertEquals(response.status_code, 200)
 
     def test_get_specific_question(self):
         """Test method that fetches specific question."""
-        response = self.client.get("http://localhost:5000/api/v1/questions/1")
+        response = self.client.get("/api/v1/questions/1")
         self.assertEquals(response.status_code, 200)
 
     def test_post_a_question(self):
         """Test method that posts a question"""
         sample = {"id": 9, "title": "How do endpoints work?"}
-        response = self.client.post("http://localhost:5000/api/v1/questions",
+        response = self.client.post("/api/v1/questions",
                                     data = json.dumps(sample),
                                     content_type = 'application/json')
-        self.assertAlmostEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 200)
+
+    def test_add_answer_to_question(self):
+        """Test method to add answers."""
+        sample_answer = {"id": 10, "ans": "this is correct"}
+        response = self.client.post("/api/v1/questions/3/answers",
+                                    data = json.dumps(sample_answer),
+                                    content_type = 'application/json')
+        self.assertEquals(response.status_code, 200)
 
 
 if __name__ == '__main__':
