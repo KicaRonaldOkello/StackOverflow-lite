c4758fe85fb718a6045164123f3c9105fefcfe21
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-08-21 18:08:07 +0300

    Add more endpoints

--- api/app/models.py
+++ api/app/models.py
@@ -12,12 +12,25 @@ class questions:
             "description": request.json["description"]
     }
         if data["title"]== "":
-            return make_response(jsonify({"error": "Empty question title"}), 404)
+            return make_response(jsonify({"error": "Empty question title"}), 204)
         if data["description"] == "":
-            return make_response(jsonify({"Error": "Empty question body"}), 404)
+            return make_response(jsonify({"Error": "Empty question body"}), 204)
         else:
             QUESTION.append(data)
-            return make_response(jsonify({'question': QUESTION}), 201)
+            return make_response(jsonify({'question': data}), 201)
+
+    def get_question(self):
+        return make_response(jsonify({'questions': QUESTION}), 200)
+
+    def get_one_question(self, questionId):
+        if type(questionId) == int:
+            if questionId <= len(QUESTION):
+                oneQ = [k for k in QUESTION if k["id"] == questionId]
+                return make_response(jsonify({'question': oneQ}), 200)
+            else:
+                return make_response(jsonify({"Error": "Question does not exist"}), 204)
+        if type(questionId) == chr:
+            return make_response(jsonify({"Error": "Wrong question id. Use integer"}), 405)
 
 
 
--- api/app/views.py
+++ api/app/views.py
@@ -7,5 +7,13 @@ quest = questions()
 
 @app.route("/api/v1/questions", methods =["POST"])
 def createQ():
-    
     return quest.post_question()
+
+@app.route("/api/v1/questions", methods =["GET"])
+def getQ():
+    return quest.get_question()
+
+@app.route("/api/v1/questions/<int:questionId>")
+def getaQ(questionId):
+    return quest.get_one_question(questionId)
+
